2fa026e0a994349fe7c385e9dbcf2ea90bfd518a
nacutac <nacutac@gmail.com>
2022-09-04 03:38:47 +0300

    Moved Dismisible into his own file

---
+++ lib/shopping_list/widgets/dismissible_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobi_lab_shopping_list_app/models/shopping_model.dart';
+import 'package:mobi_lab_shopping_list_app/shopping_list/widgets/shopping_list_tile.dart';
+
+import '../database/bloc/bloc.dart' show DatabaseBloc, DatabaseRemoveOne;
+
+class DismisibleWidget extends StatelessWidget {
+  const DismisibleWidget(
+      {super.key, required this.listToShop, required this.index});
+  final List<ShoppingModel> listToShop;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Dismissible(
+        key: Key(listToShop[index].id),
+        onDismissed: (direction) {
+          if (direction == DismissDirection.endToStart) {
+            //remove the item
+            context
+                .read<DatabaseBloc>()
+                .add(DatabaseRemoveOne(shopItemToDelete: listToShop[index]));
+            listToShop.removeAt(index);
+          }
+        },
+        direction: DismissDirection.endToStart,
+        confirmDismiss: (direction) async {
+          return showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Delete Confirmation'),
+                content: const Text(
+                  'Are you sure you want to delete this item?',
+                  style: TextStyle(color: Colors.black),
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(true),
+                    child: const Text('Delete'),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(false),
+                    child: const Text('Cancel'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        // add background when is dismiss the item.
+        background: ColoredBox(
+          color: Colors.red,
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Icon(Icons.delete, color: Colors.white),
+                Text(
+                  'Move to trash',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+        child: SelectableListTile(
+          key: ValueKey(listToShop[index]),
+          shoppingModel: listToShop[index],
+          oddNumber: index,
+        ),
+      ),
+    );
+  }
+}
--- lib/shopping_list/widgets/multiple_selection.dart
+++ lib/shopping_list/widgets/multiple_selection.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mobi_lab_shopping_list_app/models/shopping_model.dart';
 import 'package:mobi_lab_shopping_list_app/shopping_list/database/bloc/bloc.dart';
+import 'package:mobi_lab_shopping_list_app/shopping_list/widgets/dismissible_widget.dart';
 import 'package:mobi_lab_shopping_list_app/shopping_list/widgets/list_buttons.dart';
 import 'package:mobi_lab_shopping_list_app/shopping_list/widgets/shopping_list_tile.dart';
 
@@ -29,65 +30,10 @@ class _MultipleSelectItemsState extends State<MultipleSelectItems> {
       buildDefaultDragHandles: false,
       children: [
         for (var i = 0; i < listToShop.length; i++)
-          // for (final itemList in listToShop)
-          Dismissible(
-            key: Key(listToShop[i].id),
-            onDismissed: (direction) {
-              if (direction == DismissDirection.endToStart) {
-                //remove the item
-                context
-                    .read<DatabaseBloc>()
-                    .add(DatabaseRemoveOne(shopItemToDelete: listToShop[i]));
-                listToShop.removeAt(i);
-              }
-            },
-            direction: DismissDirection.endToStart,
-            confirmDismiss: (direction) async {
-              return showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text('Delete Confirmation'),
-                    content: const Text(
-                      'Are you sure you want to delete this item?',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () => Navigator.of(context).pop(true),
-                        child: const Text('Delete'),
-                      ),
-                      TextButton(
-                        onPressed: () => Navigator.of(context).pop(false),
-                        child: const Text('Cancel'),
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-            // Show a red background as the item is swiped away.
-            background: ColoredBox(
-              color: Colors.red,
-              child: Padding(
-                padding: const EdgeInsets.all(15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: const [
-                    Icon(Icons.delete, color: Colors.white),
-                    Text(
-                      'Move to trash',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            child: SelectableListTile(
-              key: ValueKey(listToShop[i]),
-              shoppingModel: listToShop[i],
-              oddNumber: i,
-            ),
+          DismisibleWidget(
+            key: ValueKey(listToShop[i].id),
+            index: i,
+            listToShop: listToShop,
           ),
         if (doneList.isNotEmpty)
           const ListButtons(
